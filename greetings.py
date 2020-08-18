682c71794f95be3c61984edd60bcaab2e809a012
Katherine Frain <katherfrain@gmail.com>
2020-08-18 15:49:15 -0400

    testing

---
+++ greetings.py
@@ -0,0 +1,4 @@
+username = input("Heyo kid. What's your name?")
+print(f"Nice name, {username}")
+uppername = username.upper()
+print(f"Did you know your name has {len(username)} letters, {uppername}?")
