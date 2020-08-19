a2e6d3ff6d35c2b64399a372bbcf7ab6cd75ddec
Katherine Frain <katherfrain@gmail.com>
2020-08-19 16:39:01 -0400

    mult

---
+++ multiplicationtables.py
@@ -0,0 +1,15 @@
+x = 0    
+y = 0 
+
+while x < 10:
+    x = x + 1
+    y = 0
+    while y <= 10:
+        print(f"{x} x {y} is {x*y}")     
+        y = y + 1  
+          
+    
+        
+
+
+        
